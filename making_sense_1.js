396431b712eb5b5b60773f257a1bc52a49f2dec1
Noemi Girch <noemitotos@yahoo.com>
2017-09-18 11:21:04 +0100

    fixed .js errors

--- making_sense_1.js
+++ making_sense_1.js
@@ -1,26 +1,35 @@
-for ( i = 1; i <= 100; i++) {
-    } if (i % 3 === 0) {
-        console.log("Sense");
-    } else if (i % 5 === 0) {
-        console.log("Making");
-    } else if (i % 5 === 0 && i % 3 === 0) {
-        console.log("Making Sense");
-    } else {
-        console.log(i);
-    }
-};
+var ul = document.getElementById("makingsense-normal");
+for (var i = 1; i <= 100; i++) {
+  if (i % 5 === 0 && i % 3 === 0)
+    msg = "Making Sense";
+  else if (i % 3 === 0)
+    msg = "Sense";
+  else if (i % 5 === 0)
+    msg = "Making";
+  else
+    msg = i;
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(msg));
+  li.setAttribute("class", msg);
+  ul.appendChild(li);
+}
 
-for ( i = 1; i <= 100; i++) {
+var ul = document.getElementById("makingsense-changed");
+for (var i = 1; i <= 100; i++, msg = "") {
 var words = ["Making","Sense"];
-    if (i % 5 === 0 && i % 3 === 0) {
-        console.log(words.join(" "));
-    } else if (i % 3 === 0) {
-    		var nomaking = words.slice(1);
-        console.log(nomaking);
-    } else if (i % 5 === 0) {
-        var nosense = words.slice(0);
-        console.log(nosense);
-    } else {
-        console.log(i);
-    }
-};
+var nomaking = words.slice(1);
+var nosense = words.slice(0,1);
+  if (i % 5 === 0 && i % 3 === 0)
+    msg = words.join(" ");
+  else if (i % 3 === 0)
+    msg = nomaking;
+  else if (i % 5 === 0)
+    msg = nosense;
+  else
+    msg = i;
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(msg));
+  li.setAttribute("class", msg);
+  ul.appendChild(li);
+}
+
